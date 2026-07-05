1a1cf97c274f1dd5bb919a098a9755d1d98b6081
zaranawan39-hub <zaranawan39@gmail.com>
2026-07-05 15:27:47 +0500

    Add script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function () {
+    console.log("Welcome to VARVON 🚀");
+
+    const button = document.querySelector("button");
+
+    button.addEventListener("click", function () {
+        alert("Welcome to VARVON! More amazing features are coming soon.");
+    });
+});
